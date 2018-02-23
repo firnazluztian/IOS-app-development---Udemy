86e253e8317a087ca35e82b4ccc0219b5e83682b
Firnaz Luztian Adiansyah <firnazluztian@ilukes-macbook.local>
2018-02-24 00:09:30 +0700

    adding iam rich - section2

---
+++ I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Firnaz Luztian Adiansyah on 23/02/18.
+//  Copyright © 2018 Firnaz Luztian Adiansyah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
