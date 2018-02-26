b8521c2c54b0639e7a4f33e268c96a3bfd0415c9
Firnaz Luztian Adiansyah <firnazluztian@ilukes-macbook.local>
2018-02-26 22:03:46 +0700

    adding motion sensor functionality to the magic8ball app

--- Magic 8 Ball/Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/Magic 8 Ball/ViewController.swift
@@ -10,9 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // initialize image view
+    @IBOutlet weak var imageView1: UIImageView!
+    
+    // array of image view balls
+    let ballArray = ["ball1", "ball2", "ball3", "ball4", "ball5"]
+    
+    var randomBallNumber = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        updateImageView()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +30,24 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    // handle action if ask button is pressed
+    @IBAction func askButtonPressed(_ sender: UIButton) {
+        
+        updateImageView()
+    }
+    
+    func updateImageView() {
+        
+        // initialize random number from 0-4
+        randomBallNumber = Int(arc4random_uniform(5))
+        
+        // update imageview with a random assigned number
+        imageView1.image = UIImage(named: ballArray[randomBallNumber])
+    }
+    
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        
+        updateImageView()
+    }
 }
 
